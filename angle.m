5837e72c18ec9bd406db497fcb49376af30e9eef
Mahmut Bulut <scienceblock@gmail.com>
2011-06-02 22:14:07 +0300

    Angle & Cross Product rev.2

--- angle.m
+++ angle.m
@@ -1,22 +1,22 @@
 function y=angle(a,b)
 
+% Ingilizce'ye cevirdim error handler ekledim
 
 if length(a) >= length(b)
-    
     son = length(a);
 else 
     son = length(b);
 end
 
 
-top=0;
+total=0;
 norm1=0;
 norm2=0;
 
 
 for i=1:son
     
-    top = top + (a(i)*b(i));
+    total = total + (a(i)*b(i));
     norm1 = norm1 + a(i)^2;
     norm2 = norm2 + b(i)^2;   
 end
@@ -25,7 +25,7 @@ end
 norm1 = sqrt(norm1);
 norm2 = sqrt(norm2);
 
-ang = acos(top/(norm1*norm2));
+ang = acos(total/(norm1*norm2));
 ang = (ang*360)/(2*pi);
 
 y = ang;
